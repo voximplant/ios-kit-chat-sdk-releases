fd59f8877973e9d8d69162e0edb50141e44e406c
Maxim Kuznetsov <mkuznetsov@voximplant.com>
2025-01-16 14:11:37 +0300

    Voximplant Kit Chat Core SDK 1.0.1 release

--- Package.swift
+++ Package.swift
@@ -7,13 +7,11 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-    	.library(name: "VoximplantKitChat", targets: ["VoximplantKitChat"]),
-    	.library(name: "VoximplantKitChatUI", targets: ["VoximplantKitChatUI", "VoximplantKitChat"]),
+        .library(name: "VoximplantKitChat", targets: ["VoximplantKitChat"]),
     ],
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "VoximplantKitChat", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-kit-chat/core/1.0.0/swiftpm/VoximplantKitChat.zip", checksum: "bf1886fdf30919f6e2a3a099169fb4d566a1097d1f100da4325b90afe556c03b"),
-        .binaryTarget(name: "VoximplantKitChatUI", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-kit-chat/ui/1.0.0/swiftpm/VoximplantKitChatUI.zip", checksum: "4d1174d91dcee7a70cbbd3904702fd44cdfb7c7bfd75428f1bc8bc8124b1f9f0"),
+        .binaryTarget(name: "VoximplantKitChat", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-kit-chat/core/1.0.1/swiftpm/VoximplantKitChat.zip", checksum: "781040e70f9e399bfd741297941f930bac218dc6b0b96d91d8bc5559844776f5"),
     ]
 )
