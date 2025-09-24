d05dc2619bcbef6e0e4bd5295a0346c89bfe415d
Maxim Kuznetsov <mkuznetsov@voximplant.com>
2025-09-24 18:47:49 +0300

    Voximplant Kit Chat SDK 1.2.1 release

--- Package.swift
+++ Package.swift
@@ -12,6 +12,6 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "VoximplantKitChat", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-kit-chat/core/1.2.0/swiftpm/VoximplantKitChat.zip", checksum: "e225b89fd945fd892e3aa1385c6af914ce3cf9da033fe56d3c98c9634aebc411"),
+        .binaryTarget(name: "VoximplantKitChat", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-kit-chat/core/1.2.1/swiftpm/VoximplantKitChat.zip", checksum: "d9b3796e40f79823527e4b237058c720a22ee22a379258ab86fbc8a391488352"),
     ]
 )
